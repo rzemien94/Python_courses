87eb1c076788eaf0957c1a176a4a8487ba63f808
rzemien94 <rzemieniewski94@gmail.com>
2022-08-09 19:03:13 +0200

    adding Bootstrap

--- DjangoBackendWebDevelopment/portfolio/portfolio/urls.py
+++ DjangoBackendWebDevelopment/portfolio/portfolio/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('somte_text', views.about,name='about'),
+    path('some_text', views.about,name='about'),
     path('contact/', views.contact,name='contact'),
     path('employee/',include('employee.urls')),
 ]
