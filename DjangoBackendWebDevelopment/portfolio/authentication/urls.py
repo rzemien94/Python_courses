fc23ad16cc46f9256645083a65d0486231bcbbdd
rzemien94 <rzemieniewski94@gmail.com>
2022-08-18 20:25:12 +0200

    adding login possibility using django method

--- DjangoBackendWebDevelopment/portfolio/authentication/urls.py
+++ DjangoBackendWebDevelopment/portfolio/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login,name='login'),
-    path('registration', views.registration,name='registration'),
+    path('', views.authlogin,name='login'),
+    path('registration', views.authregistration,name='registration'),
     path('forget-password', views.forget_password,name='forget_password'),
 ]
--- DjangoBackendWebDevelopment/portfolio/authentication/views.py
+++ DjangoBackendWebDevelopment/portfolio/authentication/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
-def login(request):
+def authlogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('profile')
+        else:
+            print('Invalid username or password')
+
     return render(request,'authentication/login.html')
 
-def registration(request):
+def authregistration(request):
     return render(request,'authentication/registration.html')
 
 def forget_password(request):
