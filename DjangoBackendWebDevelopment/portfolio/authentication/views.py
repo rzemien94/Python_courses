fe021d81a72a4772e599c00850a163ceebe16fe7
rzemien94 <rzemieniewski94@gmail.com>
2022-08-27 22:06:36 +0200

    adding registration possibility using django method and creating new project

--- DjangoBackendWebDevelopment/portfolio/authentication/views.py
+++ DjangoBackendWebDevelopment/portfolio/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def authlogin(request):
@@ -18,10 +19,28 @@ def authlogin(request):
 
 def authlogout(request):
     logout(request)
-    messages.success(request, 'Logout successfully!') 
+    messages.success(request, 'Logout successfully!')
     return redirect('login')
 
 def authregistration(request):
+    if request.method == 'POST':
+        username = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'User already exists')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email is already taken')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.error(request, 'Password and Confirm Password Not Mached')
+
+
     return render(request,'authentication/registration.html')
 
 def forget_password(request):
